1845577dd28e149b7589f6a0b89787d5083ec286
Jaime Mamani Navarro <jmamani503@gmail.com>
2021-10-02 18:18:51 -0400

    base de datos creado

---
+++ app/Http/Controllers/ProjectEnterpriseController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProjectEnterprise;
+use Illuminate\Http\Request;
+
+class ProjectEnterpriseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProjectEnterprise  $projectEnterprise
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProjectEnterprise $projectEnterprise)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProjectEnterprise  $projectEnterprise
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProjectEnterprise $projectEnterprise)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProjectEnterprise  $projectEnterprise
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProjectEnterprise $projectEnterprise)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProjectEnterprise  $projectEnterprise
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProjectEnterprise $projectEnterprise)
+    {
+        //
+    }
+}
