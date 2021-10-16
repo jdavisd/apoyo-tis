297b6889dfacea1d1db63e6ceef0c4bd243e9bb5
Usuario <josuechoque394@gmail.com>
2021-10-16 17:51:51 -0400

    laravel permisos creacion y administracion de roles3

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -16,15 +16,15 @@ class RoleSeeder extends Seeder
     {
         $admin=Role::create(['name'=>'Admin']);
         $consultor=Role::create(['name'=>'Consultor']);
-        Permission::create(['name'=>'admin.home','description'=>'administracion ventana principal'])->syncRoles(['admin', 'consultor']);
-        Permission::create(['name'=>'user.home','description'=>'administracion de usuarios'])->syncRoles(['admin']);
-        Permission::create(['name'=>'empresa','description'=>'registrar y administrar empresas'])->syncRoles(['admin']);
+        Permission::create(['name'=>'admin.home','description'=>'administracion ventana principal'])->syncRoles(['Admin', 'Consultor']);
+        Permission::create(['name'=>'user.home','description'=>'administracion de usuarios'])->syncRoles(['Admin']);
+        Permission::create(['name'=>'empresa','description'=>'registrar y administrar empresas'])->syncRoles(['Admin']);
        /* Permission::create(['name'=>'admin.roles.create','description'=>'registrar y administrar empresas']);
         Permission::create(['name'=>'admin.roles.destroy','description'=>'registrar y administrar empresas']);
         Permission::create(['name'=>'admin.roles.destroy','description'=>'registrar y administrar empresas']);
         Permission::create(['name'=>'admin.roles.update','description'=>'registrar y administrar empresas']);
         Permission::create(['name'=>'admin.roles.show ','description'=>'registrar y administrar empresas']);*/
-        Permission::create(['name'=>'admin.roles','description'=>'registrar y administrar roles'])->syncRoles(['admin']);
+        Permission::create(['name'=>'admin.roles','description'=>'registrar y administrar roles'])->syncRoles(['Admin']);
 
 
 
