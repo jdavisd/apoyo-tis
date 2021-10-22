8f566aa795c2cfe94640a91fbaaaa7de8b103fdf
Usuario <josuechoque394@gmail.com>
2021-10-22 18:42:14 -0400

    import de usuarios

--- app/Imports/UserImport.php
+++ app/Imports/UserImport.php
@@ -6,8 +6,9 @@ use App\Models\User;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
 
-class UserImport implements ToModel,WithHeadingRow
+class UserImport implements ToModel,WithHeadingRow,WithValidation
 {
     public $roles;
         
@@ -22,13 +23,26 @@ class UserImport implements ToModel,WithHeadingRow
     */
     public function model(array $row)
     {
+        $partsEmail= explode("@", strval( $row['email']));
+        //$partsName= explode(" ", strval( $row['name']));
+        //substr(string,start,length)
+        $partname=substr(strval( $row['name']),0,3);
 
         $user=  User::create([
             'name'=>$row['name'],
             'email'=>$row['email'],
-            'password'=>Hash::make($row['password']),
+            //'password'=>Hash::make($row['password']),
+            //'password'=>Hash::make($codeSis),
+            'password'=>Hash::make($partsEmail[0].$partname),
         ]);
         $user->roles()->sync($this->roles);
         return $user;
+    }
+    public function rules(): array
+    {
+        return [
+            'name' => ['required','string',],
+            'email' => ['required','string',],
+        ];
     }
 }
