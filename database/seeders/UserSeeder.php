bdf3900702e5a12edf6d9c07aca9617058ee9726
Usuario <josuechoque394@gmail.com>
2021-10-25 09:30:27 -0400

    preparando subir mensajes3

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,7 +17,7 @@ class UserSeeder extends Seeder
     public function run()
     {
         //
-        User::create(['name'=>'admin','email'=>'admin@admin.com','password'=>Hash::make('admin@admin.com')])->assignRole('admin');
+        User::create(['name'=>'admin','email'=>'admin@admin.com','password'=>Hash::make('admin@admin.com')])->assignRole('Admin');
         User::create(['name'=>'consultor','email'=>'consultor@consultor.com','password'=>Hash::make('consultor@consultor.com')])->assignRole('Consultor');
         Adviser::create(['name'=>'adviser']);
 
