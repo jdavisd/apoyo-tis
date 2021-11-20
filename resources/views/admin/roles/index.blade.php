d791b7dec8e5a505b9f31fe93fce21dbab2f51e4
Usuario <josuechoque394@gmail.com>
2021-11-19 21:37:28 -0400

    mensajes errores

--- resources/views/admin/roles/index.blade.php
+++ resources/views/admin/roles/index.blade.php
@@ -14,41 +14,7 @@
         <strong>{{session('info')}}</strong>
     </div>
 @endif
-  <div class="card">
-    <div class="card-body">
-        <table class="table table-striped"
-          <thead class="thead-light">
-               <tr>
-                   <th>ID</th>
-                   <th>Rol</th>
-                   <th colspan="2"></th>
-                
-               </tr>
-           </thead>
-           <tbody>
-               @foreach ($roles as $role)
-               <tr>
-                <td>{{$role->id}}</td>
-                <td>{{$role->name}}</td>
-                <td  width="10px"><a class ="btn btn-sm btn-primary" href="{{route('admin.roles.edit',$role)}}">Editar</a></td>
-                
-                    @if ($role->name!='Admin')
-                    <td width="10px">  
-                    <form action="{{route('admin.roles.destroy',$role)}}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-sm btn-danger">Eliminar</button>
-                        </form></td>
-                    @endif
-             
-              </tr>
-               @endforeach
-            
-           </tbody>
-       </table>
-        </table>
-     </div>
-  </div>
+@livewire('admin.roles-index')
 @stop
 
 @section('css')
